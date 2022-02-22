90a2e3321e57bcf7fc1dffe82f1f8cf7f8284b1a
wangshanghai240 <tmdwangwashanghaitmd@outlook.com>
2022-02-22 10:23:14 +0800

    'x'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import Vue from 'vue'
 // 导入echarts
 import echarts from './echarts/echarts'
 // 挂载echarts到Vue.prototype
-Vue.prototype.$echarts = echarts
+Vue.prototype.$echarts = echarts;
 
 createApp(App).mount('#app')
